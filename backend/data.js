97962cfac535b402800950fb580556cf956e6e6d
Ellin <ellinyumiko@gmail.com>
2024-09-03 12:01:54 +0630

    Eco Project

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+const data = {
+    products: [
+        {
+        name: "LV 2024",
+        slug: "lv-2024",
+        price: "$20000",
+        decription: "Brand_New",
+        image: "/LuxuryBags/B4.png"
+    },
+    {
+        name : "Chanel 2024",
+        slug : "chanel-2024",
+        price : "$24000",
+        decription : "Brand_New",
+        image : "/LuxuryBags/B1.png"
+    },
+    {
+        name : "Forever21 2024",
+        slug : "forever21-2024",
+        price : "$26000",
+        decription : "Brand_New",
+        image : "/LuxuryBags/B3.png"
+    },
+    {
+        name : "UnderArmor 2024",
+        slug : "underarmor-2024",
+        price : "$14000",
+        decription : "Brand_New",
+        image : "/LuxuryBags/B2.png"
+    }
+]
+} 
+
+module.exports = data
